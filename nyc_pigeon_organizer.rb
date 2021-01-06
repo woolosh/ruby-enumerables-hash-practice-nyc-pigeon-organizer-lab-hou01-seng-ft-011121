c919fc0adf7188f6847f0d40fe8ee942d746c884
Joshua Morales <joshuapmorales@gmail.com>
2021-01-06 01:00:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,7 +13,7 @@ def nyc_pigeon_organizer(data)
         if !final_array[name][key]
           !final_array[name][key] = []
         end
-        final_array[name][key].push(inner_key)
+        final_array[name][key].push(inner_key.to_s)
       end
     end
   end
