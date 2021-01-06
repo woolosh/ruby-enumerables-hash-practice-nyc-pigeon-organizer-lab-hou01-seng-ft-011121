df2d847c4b5dfcdc1b2d34986b4572edd143c541
Joshua Morales <joshuapmorales@gmail.com>
2021-01-06 00:55:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
 
-  data.each_with_object({}) do |(key, value), final_array|
+  final_results = data.each_with_object({}) do |(key, value), final_array|
     # binding.pry
     value.each do |inner_key, names|
       names.each do |name|
@@ -13,10 +13,10 @@ def nyc_pigeon_organizer(data)
         if !final_array[name][key]
           !final_array[name][key] = []
         end
-        final_array[name][key].push
+        final_array[name][key].push(inner_key)
       end
     end
-    final_array
+    # final_array
    end    
   binding.pry
 end
