e8fb4be17f885eb911e2fb73248a4d93ed9ec861
Joshua Morales <joshuapmorales@gmail.com>
2021-01-06 00:47:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  data.each_with_object({}) do |(key, value), final_array|
+    # binding.pry
+    value.each do |inner_key, names|
+      binding.pry
+      names.each do |name|
+        
+    end
+    final_array
+   end    
+  binding.pry
 end
